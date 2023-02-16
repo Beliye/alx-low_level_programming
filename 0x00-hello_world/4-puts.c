0b0196e06079356de5183117912aaeb7ed94d8c1
Beliye <lula.aster@gmail.com>
2023-02-16 04:06:34 -0800

    d

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ /**
+  * main -  I am now calling u
+  * Return:  0 (Success)
+  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
