ee34ec793eb9958543aa50727922812b86315301
Beliye <lula.aster@gmail.com>
2023-02-17 04:31:21 -0800

    s

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n < 100; n++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-
+	}
 	putchar('\n');
 	return (0);
 }
