38b5454eea237f8f26a5b14e971093c285253484
Beliye <lula.aster@gmail.com>
2023-03-10 13:11:33 -0800

    s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
  * main - prints its name
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 if successful
  */
-int main(int argc attribute((unused)), char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
